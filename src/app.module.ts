86421c426aa1649916b54f5694e4ee3a5980d389
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2025-07-18 09:05:13 -0300

    Revert "Created JWT/AUTH for token validation and Users module to create a user and login"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule,
-    PrismaModule,
-    UsersModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
